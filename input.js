72373ea5215252e6292df4c77fa71cdf26a29d07
solomon-eke <ekesolomon11@gmail.com>
2021-11-04 02:43:52 +0000

    WASD

--- input.js
+++ input.js
@@ -4,8 +4,28 @@ const handleUserInput = function (key) {
     process.exit();
   }
 };
+const snakeMove = function (key) {
+  if (key === "w") {
+    stdin.on("data", (input) => {
+      client.write("Move: up");
+    });
+  } else if (key === "a") {
+    stdin.on("data", (input) => {
+      client.write("Move: left");
+    });
+  } else if (key === "s") {
+    stdin.on("data", (input) => {
+      client.write("Move: down");
+    });
+  } else if (key === "d") {
+    stdin.on("data", (input) => {
+      client.write("Move: right");
+    });
+  }
+};
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
